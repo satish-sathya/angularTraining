65301a9a2f7c4c8081f46019c4621339ee11cdcd
Yashwanth Reddy <yashwanth.yenugu@gmail.com>
2018-07-17 21:48:07 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import{HomeComponent} from './home/index'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  // directives: [HomeComponent]
+})
+export class AppComponent {
+  title = 'app works!';
+}
